3ffe970dbd84c66b0cd187ee31c3ae41579c3993
neizerjemima <113371239+neizerjemima@users.noreply.github.com>
2024-02-28 12:58:07 -0500

    done

--- js/article_lightbox.js
+++ js/article_lightbox.js
@@ -6,7 +6,16 @@
         
     }
 
-articleNews.forEach(function(info) {
-info.addEventListener("click", newsContent)
-});
+    let article = {
+        picture: "images/img1.jpg",
+        heading: "The Impact of Mental Health Stigma",
+        subHeading: "Impact",
+        description:
+            "For my personalproject, I took on the challenge of refreshing the Quatro brand, a popular drink from the 80s that was famous in the UK. The main goal was to give the brand a new look and feel, and to create a series of responsive websites that would scale seamlessly across different devices. To achieve this, I designed three websites: the index page, shop page, and contact page. The index page serves as the main landing page for the brand, providing an overview of its history and products. I carefully curated the content and design elements to create an engaging and informative user experience.",
+        Date: "21st May, 2024",
+        };
+
+// articleNews.forEach(function(info) {
+// info.addEventListener("click", newsContent)
+// });
 })();
