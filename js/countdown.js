082bbefd2a46aa7536e84b03668c717b590001a4
Sssanp <paosan.2004@gmail.com>
2024-02-28 12:45:01 -0500

    countdown gsap

--- js/countdown.js
+++ js/countdown.js
@@ -1,37 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#numberCount1", { innerText: 4000, duration: 3, 
-        scrollTrigger: {
-        trigger: "#numberCount1",
-        start: "top center",
-        end: "bottom center",
-        scrub: true,    
-        snap: {
-            innerText:1
-        } 
-    }
-  });
+// Animation for the countdown statistics
+function animateCountdown(target, endValue) {
+    gsap.to(target, {
+        innerText: endValue,
+        duration: 3,
+        ease: "power1.out"
+    });
+}
 
-  gsap.to("#numberCount2", { innerText: 11, duration: 3, 
-    scrollTrigger: {
-        trigger: "#numberCount2",
+// ScrollTrigger to trigger animation when the "statistics" section enters the viewport
+gsap.utils.toArray("#statistics").forEach(section => {
+    ScrollTrigger.create({
+        trigger: section,
         start: "top center",
         end: "bottom center",
-        scrub: true,    
-        snap: {
-            innerText:1
-        } 
-    }
-  });
-
-  gsap.to("#numberCount3", { innerText: 200, duration: 3,
-    scrollTrigger: {
-        trigger: "#numberCount3",
-        start: "top center",
-        end: "bottom center",
-        scrub: true,    
-        snap: {
-            innerText:5
-        } 
-    } 
-  });
+        onEnter: () => {
+            animateCountdown("#numberCount1", 4000);
+            animateCountdown("#numberCount2", 11);
+            animateCountdown("#numberCount3", 200);
+        }
+    });
+});
